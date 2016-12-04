708b5e5a046f28f11b9fd0f47da31b9c241c8191
mvartani76 <mike.vartanian@gmail.com>
2016-12-04 16:32:31 -0500

    Changed limit from 50 to 20 and maxlimit from 15 to 50 to show more data

--- cs50-php-app/src/Controller/Api/SensordataController.php
+++ cs50-php-app/src/Controller/Api/SensordataController.php
@@ -7,8 +7,8 @@ class SensordataController extends AppController
 {
     public $paginate = [
         'page' => 1,
-        'limit' => 5,
-        'maxLimit' => 15,
+        'limit' => 20,
+        'maxLimit' => 50,
         'sortWhitelist' => [
             'id', 'temp1', 'photo1', 'DeviceType'
         ]
